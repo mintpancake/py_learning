c97f0d38200b78e0f1e6f3b9f0bc2af4383e4abf
Dennis <bouncy.lu@hotmail.com>
2019-09-22 16:11:45 +0800

    new test

--- test/test.py
+++ test/test.py
@@ -1,32 +1,7 @@
-def rp(a, b):
-    r = True
-    n = min(a, b)
-    for i in range(2, n+1):
-        if a % i == 0 and b % i == 0:
-            r = False
-            break
-    return r
-
-n = tuple(map(int, input('Numbers: ').split()))
-mrp = False
-for i in range(len(n)):
-    for j in range(i+1, len(n)):
-        if rp(n[i], n[j]):
-            mrp = True
-            break
-    if mrp == True:
-        break
-prp = True
-for i in range(len(n)):
-    for j in range(i+1, len(n)):
-        if not rp(n[i], n[j]):
-            prp = False
-            break
-    if prp == False:
-        break
-if prp:
-    print('prp')
-elif mrp:
-    print('mrp')
-else:
-    print('~')
+list = [1, 2, 3]
+s = set([])
+for i in range(len(list)):
+    for j in range(len(list)):
+        if i != j:
+            s.add(list[i]+list[j])
+print(s)
