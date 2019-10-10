7265d55363dc9fee54edbc3d14413e1e20b3a066
Dennis <bouncy.lu@hotmail.com>
2019-10-10 10:36:38 +0800

    new test

--- test/test.py
+++ test/test.py
@@ -1,7 +1,22 @@
-list = [1, 2, 3]
-s = set([])
-for i in range(len(list)):
-    for j in range(len(list)):
-        if i != j:
-            s.add(list[i]+list[j])
-print(s)
+import random
+target = random.randint(0, 100)
+min = 0
+max = 99
+print("Guess a number", min, "-", max, ": ", end="")
+guess = int(input())
+count = 1
+while guess != target:
+    if not (guess<=max and guess>=min):
+        print('Not in the range!')
+        print("Guess a number", min, "-", max, ": ", end="")
+        guess = int(input())
+        count+=1
+        continue
+    if target<guess:
+        max=guess
+    else:
+        min=guess
+    print("Guess a number", min, "-", max, ": ", end="")
+    guess = int(input())
+    count+=1
+print('Correct after',count,'trial!')
