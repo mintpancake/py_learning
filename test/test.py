7647d468a407bd7f5066a46f0cf69f2f1460c414
Dennis <bouncy.lu@hotmail.com>
2019-10-16 14:54:57 +0800

    test for assignment 2

--- test/test.py
+++ test/test.py
@@ -1,22 +1,42 @@
-import random
-target = random.randint(0, 100)
-min = 0
-max = 99
-print("Guess a number", min, "-", max, ": ", end="")
-guess = int(input())
-count = 1
-while guess != target:
-    if not (guess<=max and guess>=min):
-        print('Not in the range!')
-        print("Guess a number", min, "-", max, ": ", end="")
-        guess = int(input())
-        count+=1
-        continue
-    if target<guess:
-        max=guess
+def show(l,size):
+    for i in range(size):
+        for j in range(size):
+            print(f'{l[size*i+j]:>2}',end='')
+            if j==size-1:
+                print()
+            else:
+                print(' ',end='')
+
+def judge(l,size):
+    for i in range(size):
+        if len(set(l[i*size:(i+1)*size]))==1:
+            print('Winner:',l[i*size])
+            return True
+        elif len(set(l[i::size]))==1:
+            print('Winner:',l[i])
+            return True
+    if len(set(l[0::size+1]))==1:
+        print('Winner:',l[0])
+        return True
+    elif len(set(l[size-1:size**2-2:size-1]))==1:
+        print('Winner:',l[size-1])
+        return True
+    return False
+
+size=int(input('Size--> '))
+l=[]
+for i in range(size):
+    for j in range(size):
+        l.append(size*i+j)
+show(l,size)
+for n in range(size**2):
+    if n%2==0:
+        x=int(input('X--> '))
+        l[x]='X'
     else:
-        min=guess
-    print("Guess a number", min, "-", max, ": ", end="")
-    guess = int(input())
-    count+=1
-print('Correct after',count,'trial!')
+        o=int(input('O--> '))
+        l[o]='O'
+    show(l,size)
+    if judge(l,size):
+        exit()
+print('Winner: None')
