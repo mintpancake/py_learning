1dc601895ef8395ec8e32be5355882d3e194dc48
Dennis <bouncy.lu@hotmail.com>
2019-12-08 19:26:21 +0800

    AI vs Human

--- test/test.py
+++ test/test.py
@@ -1,42 +1,72 @@
-def show(l,size):
-    for i in range(size):
-        for j in range(size):
-            print(f'{l[size*i+j]:>2}',end='')
-            if j==size-1:
-                print()
-            else:
-                print(' ',end='')
+board={'A':['0','1','2','3','4','5','6','7','8','9'],'B':['0','1','2','3','4','5','6','7','8','9'],'C':['0','1','2','3','4','5','6','7','8','9']}
+valid_key=['A','B','C']
 
-def judge(l,size):
-    for i in range(size):
-        if len(set(l[i*size:(i+1)*size]))==1:
-            print('Winner:',l[i*size])
-            return True
-        elif len(set(l[i::size]))==1:
-            print('Winner:',l[i])
-            return True
-    if len(set(l[0::size+1]))==1:
-        print('Winner:',l[0])
-        return True
-    elif len(set(l[size-1:size**2-2:size-1]))==1:
-        print('Winner:',l[size-1])
-        return True
-    return False
+def show():
+    for i in range(len(valid_key)-1):
+        print(f'{valid_key[i]}      ',end='')
+    print(valid_key[-1])
+    for i in range(3):
+        for j in range(len(valid_key)-1):
+            print(f'{board[valid_key[j]][3*i]} {board[valid_key[j]][3*i+1]} {board[valid_key[j]][3*i+2]}  ',end='')
+        print(f'{board[valid_key[-1]][3*i]} {board[valid_key[-1]][3*i+1]} {board[valid_key[-1]][3*i+2]}')
+def scan(n):
+    ipt=list((input(f'Player {n}: ')))
+    while len(ipt)!=2 or ipt[0] not in valid_key or (not (int(ipt[1])>=0 and int(ipt[1])<=8)) or board[ipt[0]][int(ipt[1])]=='X':
+        print('Invalid move, please input again')
+        ipt=list((input(f'Player {n}: ')))
+    return ipt
 
-size=int(input('Size--> '))
-l=[]
-for i in range(size):
-    for j in range(size):
-        l.append(size*i+j)
-show(l,size)
-for n in range(size**2):
-    if n%2==0:
-        x=int(input('X--> '))
-        l[x]='X'
+def modify(ipt):
+    board[ipt[0]][int(ipt[1])]='X'
+    return
+
+def judge_winner():
+    if len(valid_key)==1 and judge_dead_board()[0]:
+         return True
     else:
-        o=int(input('O--> '))
-        l[o]='O'
-    show(l,size)
-    if judge(l,size):
-        exit()
-print('Winner: None')
+        return False
+
+def judge_dead_board():
+    dead=False
+    dead_board=''
+    for i in board:
+        for j in range(3):
+            if board[i][3*j]==board[i][3*j+1] and board[i][3*j+1]==board[i][3*j+2]:
+                dead=True
+                dead_board=i
+                break
+            if board[i][j]==board[i][3+j] and board[i][3+j]==board[i][6+j]:
+                dead=True
+                dead_board=i
+                break
+            if (board[i][0]==board[i][4] and board[i][4]==board[i][8]) or (board[i][2]==board[i][4] and board[i][4]==board[i][6]):
+                dead=True
+                dead_board=i
+                break
+        if dead==True:
+            break
+    return dead,dead_board
+
+def ai():
+    return 'A0'
+
+def main():
+    player=1
+    while True:
+        show()
+        if player==1:
+            ipt=ai()
+        if player==2:
+            ipt=scan(player)
+        modify(ipt)
+        if judge_winner():
+            print(f'Player {player%2+1} wins game')
+            break
+        if judge_dead_board()[0]:
+            dead_board=judge_dead_board()[1]
+            valid_key.remove(dead_board)
+            del board[dead_board]
+        player=player%2+1
+    return
+
+main()
